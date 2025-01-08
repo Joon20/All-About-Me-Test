8642d1d3f4b23240947ec868a63b9fbe67c82b29
Joon20 <jypaik200174@gmail.com>
2025-01-08 23:53:21 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
       addMessage("user", message);
   
       try {
-        const response = await fetch("http://localhost:5001/chat", {
+        const response = await fetch("https://all-about-me-test.onrender.com/chat", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ message, boyfriendType }),
